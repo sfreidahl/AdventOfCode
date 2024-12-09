d925a23f5870b2844217fb590b210da706830cd6
Søren Freidahl Jensen <soeren.freidahl.jensen@LEGO.com>
2024-12-09 11:53:10 +0100

    2024-09

---
+++ 2024/09-01/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllText("input.txt").Select(x => int.Parse(x.ToString())).ToList();
+
+long result = 0;
+var index = 0;
+var memoryIndex = 0;
+var fileIndex = 0;
+var reverseIndex = input.Count - 1;
+Console.WriteLine(reverseIndex);
+if (reverseIndex % 2 == 1)
+{
+    reverseIndex--;
+}
+
+var h = input[reverseIndex];
+Console.WriteLine(h);
+
+while (true)
+{
+    var isFreeSpace = memoryIndex % 2 == 1;
+    var val = input[memoryIndex];
+    if (!isFreeSpace)
+    {
+        if (memoryIndex >= reverseIndex)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                result += fileIndex * index;
+                index++;
+            }
+            break;
+        }
+        else
+        {
+            for (int j = 0; j < val; j++)
+            {
+                result += fileIndex * index;
+                index++;
+            }
+        }
+        fileIndex++;
+    }
+    if (isFreeSpace)
+    {
+        var remainingToFill = val;
+
+        for (int j = 0; j < remainingToFill; j++)
+        {
+            result += reverseIndex / 2 * index;
+            h--;
+            if (h == 0)
+            {
+                reverseIndex -= 2;
+                h = input[reverseIndex];
+            }
+            index++;
+        }
+    }
+    memoryIndex++;
+}
+
+Console.WriteLine(result);
---
+++ 2024/09-02/Program.cs
@@ -0,0 +1,129 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllText("input.txt").Select(x => int.Parse(x.ToString())).ToList();
+
+List<Memory> memory = new();
+
+var isFreeSpace = false;
+var fileIndex = 0;
+foreach (var i in input)
+{
+    if (!isFreeSpace)
+    {
+        memory.Add(new FileSpace(fileIndex, i));
+        fileIndex++;
+    }
+    else
+    {
+        memory.Add(new FreeSpace(i));
+    }
+    isFreeSpace = !isFreeSpace;
+}
+
+// PrintMemory();
+
+var reverseIndex = input.Count - 1;
+foreach (var file in memory.Where(x => x is FileSpace).Cast<FileSpace>().Reverse())
+{
+    var indexOfFile = memory.IndexOf(file);
+    var firstSpace = memory.Find(x => x is FreeSpace && x.Size >= file.Size);
+    if (firstSpace is null)
+    {
+        continue;
+    }
+    var indexOfFreeSpace = memory.IndexOf(firstSpace);
+    if (indexOfFreeSpace > indexOfFile)
+    {
+        continue;
+    }
+    memory[indexOfFile] = new FreeSpace(file.Size);
+    if (firstSpace.Size == file.Size)
+    {
+        memory[indexOfFreeSpace] = file;
+    }
+    else
+    {
+        memory[indexOfFreeSpace] = firstSpace with { Size = firstSpace.Size - file.Size };
+        memory.Insert(indexOfFreeSpace, file);
+    }
+    // PrintMemory();
+}
+
+var index = 0;
+long result = 0;
+foreach (var m in memory)
+{
+    if (m is FileSpace file)
+    {
+        for (int i = 0; i < file.Size; i++)
+        {
+            result += file.Index * index;
+            index++;
+        }
+    }
+    if (m is FreeSpace)
+    {
+        index += m.Size;
+    }
+}
+
+Console.WriteLine(result);
+
+void PrintMemory()
+{
+    foreach (var m in memory)
+    {
+        if (m is FileSpace fileSpace)
+        {
+            for (int i = 0; i < fileSpace.Size; i++)
+            {
+                Console.Write(fileSpace.Index);
+            }
+        }
+        if (m is FreeSpace freeSpace)
+        {
+            for (int i = 0; i < freeSpace.Size; i++)
+            {
+                Console.Write(".");
+            }
+        }
+    }
+    Console.WriteLine();
+}
+
+record Memory(int Size);
+
+record FileSpace(int Index, int Size) : Memory(Size);
+
+record FreeSpace(int Size) : Memory(Size);
+
+
+
+// Console.WriteLine("big list");
+// var multiplier = 0;
+// var fileId = 0;
+// var index = 0;
+// var currentFreeSpace = 0;
+// var reverseIndex = input.Count - 1;
+// while(true){
+//     var isFreeSpace = index % 2 == 1;
+//     if(!isFreeSpace){
+//         for(int i = 0; i < input[index]; i++){
+//             result += multiplier *fileId;
+//             multiplier++;
+//         }
+//         fileId++;
+//     }else{
+//         if(currentFreeSpace == 0){
+//             currentFreeSpace = input[index];
+//         }
+
+
+//     }
+//     index++;
+// }
+
+
+
+// Console.WriteLine(result);
