a37ccad027a6a75527666b8cb02dadef4720330e
Søren Freidahl Jensen <soeren.freidahl.jensen@LEGO.com>
2024-12-17 14:10:43 +0100

    2024-17

--- 2024/16-02/Program.cs
+++ 2024/16-02/Program.cs
@@ -3,86 +3,100 @@ using System.Diagnostics;
 
 Console.WriteLine("Hello, World!");
 
-var locations = File.ReadLines("input.txt").SelectMany((row, rowIndex) => row.Select((c, columnIndex) => (c, new Coordinate(rowIndex, columnIndex)))).Where(x => x.c != '#');
+var locations = File.ReadLines("input.txt")
+    .SelectMany((row, rowIndex) => row.Select((c, columnIndex) => (c, new Coordinate(rowIndex, columnIndex))))
+    .Where(x => x.c != '#');
 
-
-var maze = new Maze(locations.ToDictionary(x => x.Item2, x=> x.c));
+var maze = new Maze(locations.ToDictionary(x => x.Item2, x => x.c));
 
 Console.WriteLine(maze.GetLocationsALongPath());
 
 ;
 
-class Maze(Dictionary<Coordinate, char> Maze){
+class Maze(Dictionary<Coordinate, char> Maze)
+{
     private Dictionary<Coordinate, char> _maze = Maze;
-    private Position _start =  new (Maze.Where(x => x.Value == 'S').First().Key, Direction.Right);
+    private Position _start = new(Maze.Where(x => x.Value == 'S').First().Key, Direction.Right);
     private Coordinate _end = Maze.Where(x => x.Value == 'E').First().Key;
     private Dictionary<Position, int> _visited = [];
 
-
-    public int GetLocationsALongPath(){
+    public int GetLocationsALongPath()
+    {
         Dictionary<Position, int> costs = [];
         List<(Position Position, int Cost, List<Coordinate> CurrentPath)> currentPositions = [(_start, 0, [])];
         _visited.Add(_start, 0);
         List<(List<Coordinate> Path, int Cost)> paths = [];
-        while(currentPositions.Count > 0){
+        while (currentPositions.Count > 0)
+        {
             Console.WriteLine(currentPositions.Count);
             currentPositions = currentPositions.SelectMany(x => GetPositionsAndPrice(x.Position, x.Cost, x.CurrentPath)).ToList();
 
-            foreach(var endPoint in currentPositions.Where(x => x.Position.Coordinate == _end).ToList()){
+            foreach (var endPoint in currentPositions.Where(x => x.Position.Coordinate == _end).ToList())
+            {
                 paths.Add((endPoint.CurrentPath, endPoint.Cost));
                 currentPositions.Remove(endPoint);
             }
-
         }
         return paths.Where(x => x.Cost == paths.MinBy(x => x.Cost).Cost).SelectMany(x => x.Path).ToHashSet().Count + 1;
-
     }
 
-    private List<(Position Position, int Cost, List<Coordinate> CurrentPath)> GetPositionsAndPrice(Position CurrentPosition, int CurrentPrice, List<Coordinate> CurrentPath){
-        if(_visited.TryGetValue(CurrentPosition, out var v)){
-            if(v < CurrentPrice){
+    private List<(Position Position, int Cost, List<Coordinate> CurrentPath)> GetPositionsAndPrice(
+        Position CurrentPosition,
+        int CurrentPrice,
+        List<Coordinate> CurrentPath
+    )
+    {
+        if (_visited.TryGetValue(CurrentPosition, out var v))
+        {
+            if (v < CurrentPrice)
+            {
                 return [];
             }
         }
         _visited[CurrentPosition] = CurrentPrice;
         List<(Position Position, int Cost, List<Coordinate> CurrentPath)> newPositions = [];
-        foreach(var dir in GetNewDirections(CurrentPosition.Direction)){
+        foreach (var dir in GetNewDirections(CurrentPosition.Direction))
+        {
             var newPos = CurrentPosition.Coordinate.GetAdjacentCoordinate(dir.Direction);
-            if(!_maze.ContainsKey(newPos)){
+            if (!_maze.ContainsKey(newPos))
+            {
                 continue;
             }
-            newPositions.Add((new (newPos, dir.Direction), CurrentPrice + dir.Cost, [..CurrentPath, newPos]));
+            newPositions.Add((new(newPos, dir.Direction), CurrentPrice + dir.Cost, [.. CurrentPath, newPos]));
         }
 
         return newPositions;
-
     }
 
-    private List<(Direction Direction, int Cost)> GetNewDirections(Direction direction){
+    private List<(Direction Direction, int Cost)> GetNewDirections(Direction direction)
+    {
         var right = direction == Direction.Left ? Direction.Up : direction + 1;
         var left = direction == Direction.Up ? Direction.Left : direction - 1;
         return [(left, 1001), (direction, 1), (right, 1001)];
     }
-
 }
 
-record Coordinate(int Row, int Column){
-    public Coordinate GetAdjacentCoordinate(Direction direction){
-        return direction switch {
-            Direction.Up => new (Row-1, Column),
-            Direction.Down => new (Row+1, Column),
-            Direction.Left => new (Row, Column - 1),
-            Direction.Right => new (Row, Column + 1),
-            _ => throw new UnreachableException("")
+record Coordinate(int Row, int Column)
+{
+    public Coordinate GetAdjacentCoordinate(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => new(Row - 1, Column),
+            Direction.Down => new(Row + 1, Column),
+            Direction.Left => new(Row, Column - 1),
+            Direction.Right => new(Row, Column + 1),
+            _ => throw new UnreachableException(""),
         };
     }
 }
+
 record Position(Coordinate Coordinate, Direction Direction);
 
-
-enum Direction {
+enum Direction
+{
     Up,
     Right,
     Down,
-    Left
+    Left,
 }
---
+++ 2024/17-02/Program.cs
@@ -0,0 +1,154 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+long a = 0;
+long b = 0;
+long c = 0;
+int pointer = 0;
+
+string program = "2,4,1,1,7,5,0,3,1,4,4,5,5,5,3,0,";
+
+List<int> instructions = [2, 4, 1, 1, 7, 5, 0, 3, 1, 4, 4, 5, 5, 5, 3, 0];
+
+string result = "";
+
+long val = 0;
+while (true)
+{
+    val *= 8;
+    Console.WriteLine($"val - {val}");
+    for (int i = 0; i <= 16; i++)
+    {
+        Console.WriteLine($"i - {i}");
+        a = val + i;
+        Run();
+        if (result == "")
+        {
+            continue;
+        }
+        if (program.EndsWith(result))
+        {
+            Console.WriteLine($"{result} - {val + i}");
+            val += i;
+            break;
+        }
+    }
+    if (program == result)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(val);
+
+void Run()
+{
+    b = 0;
+    c = 0;
+    pointer = 0;
+    result = "";
+    while (pointer < instructions.Count)
+    {
+        OpCode opCode = (OpCode)instructions[pointer];
+        long operand = instructions[pointer + 1];
+        var increasePointer = opCode switch
+        {
+            OpCode.Adv => Adv(operand),
+            OpCode.Bxl => Bxl(operand),
+            OpCode.Bst => Bst(operand),
+            OpCode.Jnz => Jnz(operand),
+            OpCode.Bxc => Bxc(operand),
+            OpCode.Out => Out(operand),
+            OpCode.Bdv => Bdv(operand),
+            OpCode.Cdv => Cdv(operand),
+            _ => throw new UnreachableException("Invalid opcode"),
+        };
+
+        if (increasePointer)
+        {
+            pointer += 2;
+        }
+        if (result.Length > program.Length)
+        {
+            pointer = int.MaxValue;
+        }
+    }
+}
+
+bool Adv(long operand)
+{
+    a /= (long)Math.Pow(2, ComboOperand(operand));
+    return true;
+}
+
+bool Bxl(long operand)
+{
+    b ^= operand;
+    return true;
+}
+
+bool Bst(long operand)
+{
+    b = ComboOperand(operand) % 8;
+    return true;
+}
+
+bool Jnz(long operand)
+{
+    if (a == 0)
+    {
+        return true;
+    }
+    pointer = (int)operand;
+    return false;
+}
+
+bool Bxc(long operand)
+{
+    b ^= c;
+    return true;
+}
+
+bool Out(long operand)
+{
+    result += $"{ComboOperand(operand) % 8},";
+    // Console.WriteLine(result);
+    return true;
+}
+
+bool Bdv(long operand)
+{
+    b = a / (long)Math.Pow(2, ComboOperand(operand));
+    return true;
+}
+bool Cdv(long operand)
+{
+    c = a / (long)Math.Pow(2, ComboOperand(operand));
+    return true;
+}
+
+long ComboOperand(long operand)
+{
+    return operand switch
+    {
+        0 or 1 or 3 or 3 => operand,
+        4 => a,
+        5 => b,
+        6 => c,
+        _ => throw new UnreachableException(),
+    };
+}
+
+public enum OpCode
+{
+    Adv,
+    Bxl,
+    Bst,
+    Jnz,
+    Bxc,
+    Out,
+    Bdv,
+    Cdv,
+}
