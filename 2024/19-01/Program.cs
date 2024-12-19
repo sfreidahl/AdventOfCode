96b1a202c26a7f0de2dec0ed07b2557d7e2c9522
Søren Freidahl Jensen <soeren.freidahl.jensen@LEGO.com>
2024-12-19 13:43:08 +0100

    2024-19

---
+++ 2024/19-01/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var towels = File.ReadAllText("input-towels.txt").Split(", ");
+var patterns = File.ReadAllLines("input-patterns.txt");
+Dictionary<string, bool> memo = [];
+
+var result = patterns.Select(HandlePattern).Where(x => x).Count();
+
+Console.WriteLine(result);
+
+bool HandlePattern(string pattern){
+    if(memo.ContainsKey(pattern)){
+        return memo[pattern];
+    }
+
+    var newPatterns = FindNextTowels(pattern);
+    if(newPatterns.Contains(string.Empty)){
+        memo[pattern] = true;
+        return true;
+    }
+    if(newPatterns.Count == 0){
+        memo[pattern] = false;
+        return false;
+    }
+    var result = newPatterns.Select(x => HandlePattern(x)).Any(x => x);
+    memo[pattern] = result;
+    return result;
+}
+
+List<string> FindNextTowels(string pattern){
+
+    List<string> newPatterns = [];
+
+    foreach(var towel in towels){
+        if(pattern.StartsWith(towel)){
+            newPatterns.Add(pattern[towel.Length..]);
+        }
+    }
+
+    return newPatterns;
+
+}
---
+++ 2024/19-02/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var towels = File.ReadAllText("input-towels.txt").Split(", ");
+var patterns = File.ReadAllLines("input-patterns.txt");
+Dictionary<string, long> memo = [];
+
+var result = patterns.Select(HandlePattern).Sum();
+
+Console.WriteLine(result);
+
+long HandlePattern(string pattern){
+    if(memo.ContainsKey(pattern)){
+        return memo[pattern];
+    }
+
+    if(pattern == string.Empty){
+        return 1;
+    }
+
+    var newPatterns = FindNextTowels(pattern);
+    if(newPatterns.Count == 0){
+        memo[pattern] = 0;
+        return 0;
+    }
+    var result = newPatterns.Select(x => HandlePattern(x)).Sum();
+    memo[pattern] = result;
+    return result;
+}
+
+List<string> FindNextTowels(string pattern){
+
+    List<string> newPatterns = [];
+
+    foreach(var towel in towels){
+        if(pattern.StartsWith(towel)){
+            newPatterns.Add(pattern[towel.Length..]);
+        }
+    }
+
+    return newPatterns;
+
+}
