d7710e9c297d77bc2c0944ed74b7761daa05a2da
Søren Freidahl Jensen <soeren.freidahl.jensen@LEGO.com>
2024-12-24 22:09:03 +0100

    2024-23

---
+++ 2024/23-01/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<HashSet<string>> originalGroups = File.ReadAllLines("input.txt")
+    .Select(x => string.Join("-", x.Split("-").Order()))
+    .Order()
+    .Select(x => x.Split("-").ToHashSet())
+    .DistinctBy(x => string.Join("-", x))
+    .ToList();
+
+var result = 0;
+while (originalGroups.Count > 0)
+{
+    var group = originalGroups.First();
+    originalGroups.Remove(group);
+    string first = group.First();
+    string last = group.Last();
+    var firstGroups = originalGroups.Where(x => x.Contains(first));
+    foreach (var f in firstGroups)
+    {
+        var test = f.Where(x => x != first).First();
+        var results = originalGroups.Where(x => x.Contains(test) && x.Contains(last));
+        foreach (var s in results)
+        {
+            HashSet<string> strings = [first, test, .. s];
+            if (strings.Any(x => x.StartsWith('t')))
+            {
+                result++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(result);
---
+++ 2024/23-02/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var connections = File.ReadAllLines("input.txt")
+    .Select(x => x.Split("-"))
+    .SelectMany(x => new List<(string PC, string ConnectTo)> { (x[0], x[1]), (x[1], x[0]) })
+    .GroupBy(x => x.PC, x => x.ConnectTo)
+    .ToDictionary(x => x.Key, x => x.Select(x => x).ToHashSet());
+
+var groups = connections.Select(x => new HashSet<string>() { x.Key }).ToList();
+
+while (groups.Count > 1)
+{
+    Console.WriteLine(groups.Count);
+    List<HashSet<string>> newGroups = [];
+
+    foreach (var group in groups)
+    {
+        foreach (var pc in connections)
+        {
+            if (group.Contains(pc.Key))
+            {
+                continue;
+            }
+            if (group.All(x => pc.Value.Contains(x)))
+            {
+                newGroups.Add([.. group, pc.Key]);
+            }
+        }
+    }
+
+    groups = newGroups.DistinctBy(x => string.Join("-", x.Order())).ToList();
+}
+
+Console.WriteLine(string.Join(",", groups.First().Order()));
