67abcc974bd465fe5fbcf8740738f64a883eb8b6
sfreidahl <soeren.freidahl.jensen@LEGO.com>
2025-12-03 10:16:36 +0100

    2025-03

---
+++ 2025/03-01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var result = File.ReadAllLines("input.txt").Select(x => x.Select(c => c - 48).ToList()).Select(FindJoltage).Sum();
+
+Console.WriteLine(result);
+
+static int FindJoltage(List<int> batteries)
+{
+    var maxLeft = batteries[..^1].Max();
+    var maxIndex = batteries.IndexOf(maxLeft);
+    var maxRight = batteries[(maxIndex + 1)..].Max();
+
+    var joltage = maxLeft * 10 + maxRight;
+
+    Console.WriteLine(joltage);
+
+    return joltage;
+}
---
+++ 2025/03-02/Program.cs
@@ -0,0 +1,19 @@
+﻿var result = File.ReadAllLines("input.txt").Select(x => x.Select(c => c - 48).ToList()).Select(x => FindJoltage(x)).Sum();
+
+Console.WriteLine(result);
+
+static long FindJoltage(List<int> batteries, int batteryCount = 12)
+{
+    if (batteryCount == 0)
+    {
+        return 0;
+    }
+    var maxLeftIndex = batteryCount - 1;
+    var maxLeft = batteries[..^maxLeftIndex].Max();
+    var maxIndex = batteries.IndexOf(maxLeft);
+    var maxRight = FindJoltage(batteries[(maxIndex + 1)..], maxLeftIndex);
+
+    var joltage = maxLeft * (long)Math.Pow(10, maxLeftIndex) + maxRight;
+
+    return joltage;
+}
