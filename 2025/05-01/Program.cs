8f00b1e90f42e152e2e0d6a573e18acd54d5aa2e
sfreidahl <soeren.freidahl.jensen@LEGO.com>
2025-12-05 10:23:29 +0100

    2025-05

---
+++ 2025/05-01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var input = File.ReadAllText("input.txt").Split(["\n\n", "\r\n\r\n"], StringSplitOptions.RemoveEmptyEntries);
+
+var ranges = input[0]
+    .Split(["\n", "\r\n"], StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => x.Split("-").Select(long.Parse).ToArray())
+    .Select(x => new Range(x[0], x[1]));
+var ingredients = input[1].Split(["\n", "\r\n"], StringSplitOptions.RemoveEmptyEntries).Select(long.Parse);
+
+var result = ingredients.Where(ingredient => ranges.Any(range => range.Contains(ingredient))).Count();
+
+Console.WriteLine(result);
+
+record Range(long Start, long End)
+{
+    public bool Contains(long value) => value >= Start && value <= End;
+}
---
+++ 2025/05-02/Program.cs
@@ -0,0 +1,38 @@
+﻿var input = File.ReadAllText("input.txt").Split(["\n\n", "\r\n\r\n"], StringSplitOptions.RemoveEmptyEntries);
+
+var ranges = input[0]
+    .Split(["\n", "\r\n"], StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => x.Split("-").Select(long.Parse).ToArray())
+    .Select(x => new Range(x[0], x[1]))
+    .OrderBy(x => x.Start);
+
+var q = new Queue<Range>(ranges);
+
+long result = 0;
+while (q.Count > 0)
+{
+    var range = q.Dequeue();
+    do
+    {
+        if (!q.TryPeek(out Range? next) || !range.Overlaps(next))
+        {
+            result += range.Count();
+            break;
+        }
+        range = range.Combine(next);
+        q.Dequeue();
+    } while (true);
+}
+
+Console.WriteLine(result);
+
+record Range(long Start, long End)
+{
+    public bool Contains(long value) => value >= Start && value <= End;
+
+    public bool Overlaps(Range range) => Contains(range.Start) || Contains(range.End);
+
+    public Range Combine(Range range) => new(Math.Min(Start, range.Start), Math.Max(End, range.End));
+
+    public long Count() => End - Start + 1;
+}
